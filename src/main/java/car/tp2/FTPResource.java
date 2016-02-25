e95456c07faa8648c51ec73d72fcc5ba4731fc17
qvandekadsye <quentin@debian-quentin>
2016-02-25 17:24:58 +0100

    la connection au serveur marche (pas l'authentification)

--- src/main/java/car/tp2/FTPResource.java
+++ src/main/java/car/tp2/FTPResource.java
@@ -1,10 +1,18 @@
 package main.java.car.tp2;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
 
 /**
  * Exemple de ressource REST accessible a l'adresse :
@@ -16,11 +24,23 @@ import javax.ws.rs.PUT;
 
 @Path("/ftp")
 public class FTPResource {
+	protected Socket sckt;
 	
 	@GET
 	@Produces("text/html")
 	public String sayHello() {
 		return "<h1>FTP Matthieu Quentin</h1>";
+	}
+	
+	@GET
+	@Path("/connect/{ip}/{port}")
+	public String connect(@PathParam("ip") String ip,@PathParam("port") int port) throws UnknownHostException, IOException
+	{
+		this.sckt = new Socket(InetAddress.getByName(ip), port);
+		if(this.sckt.isConnected())
+			return this.sayHello()+"<br/><p>La connection au serveur est établie";
+		else
+			return this.sayHello()+"<br/><p>La connection au serveur n'est pas établie";
 	}
 
 }
