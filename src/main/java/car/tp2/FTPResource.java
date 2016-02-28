24407db242d2132dce399a162335ca37eabbd58d
qvandekadsye <qvandekadsye@gmail.com>
2016-02-28 14:35:06 +0100

    gestion de la connexion en GET, avec des ratés;

--- src/main/java/car/tp2/FTPResource.java
+++ src/main/java/car/tp2/FTPResource.java
@@ -1,6 +1,12 @@
 package main.java.car.tp2;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -25,6 +31,11 @@ import javax.ws.rs.core.Response;
 @Path("/ftp")
 public class FTPResource {
 	protected Socket sckt;
+	protected InputStream is;
+	protected InputStreamReader isr;
+	protected OutputStream os;
+	protected DataOutputStream dos;
+	protected BufferedReader buff;
 	
 	@GET
 	@Produces("text/html")
@@ -41,6 +52,59 @@ public class FTPResource {
 			return this.sayHello()+"<br/><p>La connection au serveur est Ã©tablie";
 		else
 			return this.sayHello()+"<br/><p>La connection au serveur n'est pas Ã©tablie";
+	}
+	
+	@GET
+	@Path("/login/{user}/{pass}")
+	public String login (@PathParam("user") String user, @PathParam("pass")String pass) throws IOException
+	{
+		
+		if(!this.sckt.isConnected() || this.sckt.isClosed())
+			return this.sayHello()+"<p>Vous n'êtes pas connecté au serveur.</p>";
+		else
+		{
+			
+			this.write("USER "+user+"\n",this.sckt);
+			System.out.println(this.read());
+			if(this.read().startsWith("331"))
+			{
+				this.write("PASS "+pass+"\n", this.sckt);
+				System.out.println(this.read());
+				if(this.read().startsWith("230"))
+				{
+					return this.sayHello()+"<p>Connexion réussie.<br/>Bienvenue"+user+"</p>";
+				}
+				else
+					return this.sayHello()+"<p>1Erreur d'authentification";
+			}
+			else
+				return this.sayHello()+"<p>2Erreur d'authentification";
+			
+			
+		}
+	}
+	
+	public String read(){
+		try {
+			this.is = this.sckt.getInputStream();
+			this.isr = new InputStreamReader(is);
+			this.buff = new BufferedReader(isr);
+
+			String s = buff.readLine();
+			//System.out.println(s);
+			return s;
+
+		} catch (IOException e) {
+			return "iofail" ;
+		}
+	}
+	
+	public void write(String msg, Socket socket) throws IOException{
+		
+		OutputStream os = socket.getOutputStream();
+		DataOutputStream dos = new DataOutputStream(os);
+		dos.write(msg.getBytes());
+		dos.flush();
 	}
 
 }
